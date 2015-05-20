cdec64e8c142ee22d7205b996e4b702ba7c44a14
Malutek <malutek@gmail.com>
2015-05-20 16:56:53 +0200

    Basic OptionParser implementation

--- classes/OptionParser.py
+++ classes/OptionParser.py
@@ -7,32 +7,51 @@ class OptionParser:
         # TODO it can be optional...
         pass
 
+    def __check_for_arg(self, arg_name):
+        return arg_name in self.arguments and self.arguments[arg_name] is not None
+
     @property
     def is_verbose(self):
-        # TODO
-        pass
+        return self.__check_for_arg('verbose')
 
     @property
     def is_add(self):
-        # TODO
-        pass
+        return self.__check_for_arg('add')
 
     @property
     def is_diff(self):
-        # TODO
-        pass
+        return self.__check_for_arg('diff')
 
     @property
     def file_path(self):
-        # TODO
-        pass
+        # TODO Extract parsing method
+        if 'diff' in self.arguments and self.arguments['diff'] is not None:
+            return self.arguments['diff'][0]
+        else:
+            # TODO Think of better name
+            return "---"
 
     @property
     def revision_id_1(self):
-        # TODO
-        pass
+        if 'diff' in self.arguments and self.arguments['diff'] is not None:
+            return self.arguments['diff'][1]
+        else:
+            return "---"
 
     @property
     def revision_id_2(self):
-        # TODO
-        pass
+        if 'diff' in self.arguments and self.arguments['diff'] is not None:
+            return self.arguments['diff'][2]
+        else:
+            return "---"
+
+
+    # For debug
+    # def check(self):
+    #     print("is_verbose: " + str(self.arguments))
+    #     print("is_verbose: " + str(self.is_verbose))
+    #     print("is_add: " + str(self.is_add))
+    #     print("is_diff: " + str(self.is_diff))
+    #     print("file_path: " + str(self.file_path))
+    #     print("revision_id_1: " + self.revision_id_1)
+    #     print("revision_id_2: " + self.revision_id_2)
--- history.py
+++ history.py
@@ -1,10 +1,29 @@
-import sys
 from classes.HistoryHandler import HistoryHandler, HistoryHandlerException
 from classes.OptionParser import OptionParser
 
+import argparse
+import os
+import sys
 
-args = [arg for arg in sys.argv]
-opt = OptionParser(args)
+# TODO Find more appropriate place for this function
+def is_valid_file(arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return is_valid_file
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-a', '--add', nargs=1, type=is_valid_file, help='Adds a file to the program database',
+                    required=False)
+parser.add_argument('-d', '--diff', nargs=3, type=str, help='Differentiate two sets of revisions', required=False)  # TODO Add custom check to differentiate 1 string and 2 ints
+
+# TODO Have to work on that, not sure how to create an arg without a variable
+# parser.add_argument('-v', '--verbose', nargs=1, help='???',  required=False, default=argparse.SUPPRESS)
+
+args = parser.parse_args()
+
+opt = OptionParser(vars(args))
 
 if opt.is_add:
     try:
